7b456cd8a254d3efe06118070e3d4f40103fe815
Nishant <nishant2024rde@gmail.com>
2026-02-09 09:03:56 +0530

    Importing auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Auth from './pages/auth'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 function App() {
   // 2:47
   return (
     <BrowserRouter>
     <Routes>
-      <Route></Route>
+      <Route path='/' element={<Auth />}></Route>
     </Routes>
     </BrowserRouter>
   )
